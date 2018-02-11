8e7c4c015af6ed4a6fec8fc4a90a477e44b1fc2e
rohan <purohit.rohan@gmail.com>
2018-02-11 20:41:22 +0530

    Revert "msm8956-common: nuke doze"
    This reverts commit 5e4caed70e5dee83e97e2a1dc45008d3eaaa0e7c.

--- msm8956.mk
+++ msm8956.mk
@@ -154,6 +154,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
 
+# Doze mode
+#PRODUCT_PACKAGES += \
+#    XiaomiDoze
+
 # Face detection extension
 PRODUCT_PACKAGES += \
     org.codeaurora.camera
